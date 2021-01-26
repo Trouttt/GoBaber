a6ba79cce89d309d38672612cd7777591a18ae2f
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-01-26 19:29:30 -0300

    feat: register appointment

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
+const routes = Router();
+
+routes.use('/appointment', appointmentsRouter);
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 import express from 'express';
-
+import routes from './routes/index';
 const app = express();
 
 app.use(express.json());
 
-app.get("/user", (request,response) => {
-    const { name, email } = request.body
+app.use('/', routes);
 
-    const user = {
-        name,
-        email
-    };
-    
-    return response.json({ message: 'Hello world'});
-
-})
 
 app.listen(3333, () => {
     console.log("Server started on port 3333");
